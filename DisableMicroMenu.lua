7abf69b612c121ef50aad91af857175a12f0f3fe
Mark Russ <mark.f.russ@outlook.com>
2022-10-25 23:46:55 -0400

    Now repositions bags to the bottom corner, where the micro bar and bag bar used to be.

--- DisableMicroMenu.lua
+++ DisableMicroMenu.lua
@@ -1,1 +1,11 @@
 MicroButtonAndBagsBar:Hide();
+
+local function NoOp() end
+
+local function AnchorToBottomRight(self)
+	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -3, 3);
+	self.SetPoint=NoOp;
+end
+
+ContainerFrameCombinedBags:HookScript("OnShow", AnchorToBottomRight);
+ContainerFrame1:HookScript("OnShow", AnchorToBottomRight);
